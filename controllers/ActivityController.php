1f4ce1892fe9a2b834ac6f6c38610160817bf68d
Ivan <vanchelli@gmail.com>
2019-03-25 16:02:28 +0300

    forgetted controller

---
+++ controllers/ActivityController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use app\models\Activity;
+use yii\web\Application;
+
+class ActivityController extends \yii\web\Controller
+{
+
+    public $prevPage;
+
+    public function actions(){
+
+
+        $session = Yii::$app->session;
+        $this->prevPage = $session->get('prevPage');
+
+        $session->set('prevPage',
+            Yii::$app->request->url);
+
+
+    }
+    public function actionIndex()
+    {
+
+
+
+
+        return $this->render('index',
+            ['prevPage' => $this->prevPage,]
+
+        );
+    }
+
+    public function actionView(){
+
+
+        $activity = new Activity();
+
+
+        return $this->render('view',
+            [
+                'model'=> $activity, 'prevPage' => $this->prevPage,
+            ]
+        );
+    }
+
+
+    public function actionCreate()
+    {
+        //return "Создание активности";
+        $activity = new Activity();
+        if ($activity->load(Yii::$app->request->post())){
+            if ($activity->validate()) {
+                //return "Создание активности";
+                return $this->render('view',
+                    [
+                        'model'=> $activity,
+                        'prevPage' => $this->prevPage,
+                    ]
+                );
+            }
+
+        }
+
+        return $this->render('create',
+            [
+                'model'=> $activity,'prevPage' => $this->prevPage,
+            ]
+        );
+
+    }
+
+}
